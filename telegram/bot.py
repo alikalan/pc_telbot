296b9645b0183ef3adf9c115611060a789bd5b28
Ali Kalantarnia <ali.kalantarnia@mail.de>
2024-06-15 12:06:59 +0200

    bug fixes

--- telegram/bot.py
+++ telegram/bot.py
@@ -3,7 +3,6 @@ import telebot
 from utils import get_stocks, dict_to_text
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
@@ -12,10 +11,13 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda msg: True)
 def send_stocks(message):
-    stocks = get_stocks(message)
-    reply = dict_to_text(stocks.json())
+    stocks = get_stocks(message.text.strip("/"))
+
+    try:
+        reply = dict_to_text(stocks.json())
+    except:
+        reply = f"There was a problem! The response from the server was {stocks}"
+
     bot.reply_to(message, reply)
-    # for store, stock in stocks.items():
-    #     bot.reply_to(message, f"{store}, {stock}")
 
 bot.infinity_polling()
